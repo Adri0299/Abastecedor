11dd3e768c20b90d4cb43248487504fde33ac7f4
Adri0299 <113482972+Adri0299@users.noreply.github.com>
2022-10-03 23:05:21 -0600

    Tarea_semana3, desarrollo Web

---
+++ src/main/java/com/LaNegrita/LaNegritaApplication.java
@@ -0,0 +1,13 @@
+package com.LaNegrita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaNegritaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaNegritaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LaNegrita/LaNegritaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaNegrita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaNegritaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
